585eae63d75d40c8339562cf46b58ea5c5792614
yllanvfgurgel <the_yllan@hotmail.com>
2020-11-04 20:56:30 -0300

    link com api de fotos feito, criação do PhotosService

---
+++ src/app/photos/photo/photo.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+import { Photo } from './photo';
+
+const API = 'http://localhost:3000'
+
+@Injectable({ providedIn: 'root' }) //Essa propriedade faz com que esse PhotoService possa ser acessada por toda a aplicação
+export class PhotoService {
+
+    constructor(private http: HttpClient) {}
+
+    listFromUser(userName: string) {
+        
+        return this.http
+        .get<Photo[]>(`${API}/flavio/photos`);
+
+    }
+
+}
+
--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -1,10 +1,13 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+
 import { PhotoComponent } from './photo/photo.component';
 
 
 @NgModule({
     declarations: [ PhotoComponent ],
-    exports: [ PhotoComponent ]
+    exports: [ PhotoComponent ],
+    imports: [ HttpClientModule ]
 })
 
-export class PhotosModule {}
+export class PhotosModule { }
